554501e8b4d771789419d7263c2c8a9eaaba48a1
Viorel Strogoteanu <viorelss@icloud.com>
2023-03-04 21:45:55 +0100

    [sign-in] implemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,19 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, ParseIntPipe, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController{
     constructor(private authService: AuthService){}
 
-    //Qui creiamo degli endpoints
-
-    //metodo ('nome')
-    //Il ruote quindi sarà POST /auth/signup perchè abbiamo impostate nel controller il nome principale
     @Post('signup')
-    signup() {
-        return this.authService.signup();
+    signup(@Body() dto: AuthDto) {
+        return this.authService.signup(dto);
     }
 
     @Post('signin')
-    signin() {
-        return this.authService.signin();
+    signin(@Body() dto: AuthDto) {
+        return this.authService.signin(dto);
     }
 }
+
